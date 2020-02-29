bc1137b018f2875f7d94d0e516e2aec3e37c6bae
Ryan C. Durham <ryan@stagerightlabs.com>
2020-02-29 13:19:47 -0800

    Install Dank Mono font

--- rcd_umbrella/apps/rcd_web/assets/js/app.js
+++ rcd_umbrella/apps/rcd_web/assets/js/app.js
@@ -2,6 +2,7 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../css/app.css"
+import dm from "../css/dank-mono.css"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
