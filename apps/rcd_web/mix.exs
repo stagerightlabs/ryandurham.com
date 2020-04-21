422b383c09e78f12de4ebbfa0b0b55a7ea4af2e8
Ryan C. Durham <ryan@stagerightlabs.com>
2020-04-21 14:25:39 -0700

    Remove duplicate dependency

--- apps/rcd_web/mix.exs
+++ apps/rcd_web/mix.exs
@@ -43,7 +43,6 @@ defmodule RcdWeb.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
-      {:library, in_umbrella: true},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:library, in_umbrella: true}
