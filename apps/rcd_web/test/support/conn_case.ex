61477cd52fbfad00cee206eb1c4b01664f03a10d
Ryan C. Durham <ryan@stagerightlabs.com>
2020-04-28 16:32:36 -0700

    Phoenix 1.5 upgrade

--- apps/rcd_web/test/support/conn_case.ex
+++ apps/rcd_web/test/support/conn_case.ex
@@ -18,7 +18,8 @@ defmodule RcdWeb.ConnCase do
   using do
     quote do
       # Import conveniences for testing with connections
-      use Phoenix.ConnTest
+      import Plug.Conn
+      import Phoenix.ConnTest
       alias RcdWeb.Router.Helpers, as: Routes
 
       # The default endpoint for testing
