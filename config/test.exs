fb4acc19fa89d7ec5721ef7c093f86940f439436
Ryan C. Durham <ryan@stagerightlabs.com>
2020-04-28 16:57:08 -0700

    Update test configuration; move db settings to test.secret.exs

--- config/test.exs
+++ config/test.exs
@@ -1,21 +1,5 @@
 use Mix.Config
 
-# Configure your database
-config :library, Library.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "library_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
-# Configure your database
-config :rcd, Rcd.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "rcd_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :rcd_web, RcdWeb.Endpoint,
@@ -24,3 +8,7 @@ config :rcd_web, RcdWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Finally import the config/prod.secret.exs which loads secrets
+# and configuration from environment variables.
+import_config "test.secret.exs"
