b2783b669b4982540d3338c658fcff8e5b8ece61
Ryan C. Durham <ryan@stagerightlabs.com>
2020-04-29 15:03:26 -0700

    Set up bamboo for email delivery

--- config/dev.exs
+++ config/dev.exs
@@ -18,6 +18,10 @@ config :admin, Admin.Repo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
+# Email configuration
+config :admin, Admin.Email.Mailer,
+  adapter: Bamboo.LocalAdapter
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
