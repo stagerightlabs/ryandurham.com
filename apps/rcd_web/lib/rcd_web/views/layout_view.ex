5b3bfbc23696a13231d3cc66c0dfb610a4b4c6a0
Ryan C. Durham <ryan@stagerightlabs.com>
2020-05-01 16:43:09 -0700

    Set up gravatar

--- apps/rcd_web/lib/rcd_web/views/layout_view.ex
+++ apps/rcd_web/lib/rcd_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule RcdWeb.LayoutView do
   use RcdWeb, :view
+
+  def gravatar_url(%{email: email}, size \\ "80") do
+    email = String.downcase(email)|>String.trim()
+    hash = :crypto.hash(:md5, email)|> Base.encode16(case: :lower)
+
+    "https://www.gravatar.com/avatar/" <> hash <> "?s=" <> size <> "&d=mp"
+  end
 end
