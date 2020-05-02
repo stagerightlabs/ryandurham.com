fccbb40ece38b04d3e70afa0114e2f191c23d071
Ryan C. Durham <ryan@stagerightlabs.com>
2020-05-01 17:21:45 -0700

    Set up dashboard and "to do" pages.

--- apps/rcd_web/lib/rcd_web/router.ex
+++ apps/rcd_web/lib/rcd_web/router.ex
@@ -56,6 +56,9 @@ defmodule RcdWeb.Router do
     put "/users/settings/update_password", UserSettingsController, :update_password
     put "/users/settings/update_email", UserSettingsController, :update_email
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+
+    get "/dashboard", PageController, :dashboard
+    get "/todo", PageController, :todo
   end
 
   scope "/", RcdWeb do
