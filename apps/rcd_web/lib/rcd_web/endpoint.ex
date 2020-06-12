c987dc2ff3c12d71e2fa14815ab927e843fe1fad
Ryan C. Durham <ryan@stagerightlabs.com>
2020-06-12 10:55:00 -0700

    Re-enable request logger

--- apps/rcd_web/lib/rcd_web/endpoint.ex
+++ apps/rcd_web/lib/rcd_web/endpoint.ex
@@ -30,9 +30,9 @@ defmodule RcdWeb.Endpoint do
     plug Phoenix.CodeReloader
   end
 
-  # plug Phoenix.LiveDashboard.RequestLogger,
-  #   param_key: 'request_logger',
-  #   cookie_key: 'request_logger'
+  plug Phoenix.LiveDashboard.RequestLogger,
+    param_key: 'request_logger',
+    cookie_key: 'request_logger'
 
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
