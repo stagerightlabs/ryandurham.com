2616755baf4a204f20315c129421d0b273fe1277
Ryan C. Durham <ryan@stagerightlabs.com>
2020-06-16 20:58:04 -0700

    Add colophon and revise footer

--- apps/rcd_web/lib/rcd_web/controllers/page_controller.ex
+++ apps/rcd_web/lib/rcd_web/controllers/page_controller.ex
@@ -3,6 +3,9 @@ defmodule RcdWeb.PageController do
 
   alias Library.Books
 
+  @doc """
+  Render the home page.
+  """
   def index(conn, _params) do
     year = Timex.now()
       |>Timex.format!("{YYYY}")
@@ -12,11 +15,24 @@ defmodule RcdWeb.PageController do
     render(conn, "index.html", year: year, readings: readings)
   end
 
+  @doc """
+  Render the dashboard.
+  """
   def dashboard(conn, _params) do
     render(conn, "dashboard.html")
   end
 
+  @doc """
+  Render the todo page.
+  """
   def todo(conn, _params) do
     render(conn, "todo.html")
+  end
+
+  @doc """
+  Render the colophon page.
+  """
+  def colophon(conn, _params) do
+    render(conn, "colophon.html")
   end
 end
